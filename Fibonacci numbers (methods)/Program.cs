4c0981f022947cee0a863440368d96d29ebde349
user <user@WIN-IUFBF6S61H2>
2024-12-15 21:39:04 +0200

    Add project files.

---
+++ Fibonacci numbers (methods)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fibonacci_numbers__methods_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first_number = 0, second_number = 1;
+            Console.WriteLine("Input number:");
+            int number = int.Parse(Console.ReadLine());
+            number = find_Fibonacci_numbers(first_number, second_number,number);
+            Console.Write($"{number}");
+        }
+        static int find_Fibonacci_numbers( int first_number, int second_number, int number)
+        {
+            int temt;
+            if (number == 1)
+                return 0;
+            if (number == 2) 
+                return 1; 
+            for (int i = 2; i < number; i++)
+            {
+                temt = first_number + second_number;
+                first_number = second_number;
+                second_number = temt;
+                
+            }   
+            return second_number;
+        }
+    }
+}
